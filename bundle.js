e98def5fd74ff57d691d14ebfcb8c4215a3e7613
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-27 22:20:19 +0000

    Deploy kscript/pvz2 to github.com/kscript/pvz2.git:gh-pages

--- bundle.js
+++ bundle.js
@@ -1,7 +1,213 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 'use strict';
 
+/*! *****************************************************************************
+Copyright (c) Microsoft Corporation. All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+this file except in compliance with the License. You may obtain a copy of the
+License at http://www.apache.org/licenses/LICENSE-2.0
+
+THIS CODE IS PROVIDED ON AN *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
+WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+MERCHANTABLITY OR NON-INFRINGEMENT.
+
+See the Apache Version 2.0 License for specific language governing permissions
+and limitations under the License.
+***************************************************************************** */
+
+function __awaiter(thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+}
+
+function __generator(thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+}
+
+var Task = /** @class */ (function () {
+    function Task() {
+        this.state = 'resolved';
+        this.resolve = Promise.resolve;
+        this.reject = Promise.reject;
+    }
+    Task.prototype.init = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            var _this = this;
+            return __generator(this, function (_a) {
+                if (this.state !== 'pending') {
+                    this.state = 'pending';
+                    return [2 /*return*/, new Promise(function (resolve, reject) {
+                            _this.resolve = function () {
+                                _this.state = 'resolved';
+                                resolve();
+                            };
+                            _this.reject = function () {
+                                _this.state = 'rejected';
+                                reject();
+                            };
+                        })];
+                }
+                else {
+                    throw new Error('有任务正在执行, 请先调用实例方法resolve/reject结束它');
+                }
+            });
+        });
+    };
+    return Task;
+}());
+var execHook = function (source, hook) {
+    var rest = [];
+    for (var _i = 2; _i < arguments.length; _i++) {
+        rest[_i - 2] = arguments[_i];
+    }
+    return __awaiter(void 0, void 0, void 0, function () {
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    source.state = hook;
+                    if (!(typeof source[hook] === 'function')) return [3 /*break*/, 2];
+                    return [4 /*yield*/, source[hook].apply(source, rest)];
+                case 1:
+                    _a.sent();
+                    _a.label = 2;
+                case 2: return [2 /*return*/];
+            }
+        });
+    });
+};
+
+var Scene = /** @class */ (function () {
+    function Scene(container, config) {
+        if (config === void 0) { config = {}; }
+        this.state = '';
+        this.task = new Task;
+        this.coms = [];
+        this.container = container;
+        this.config = config;
+    }
+    Scene.prototype.init = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, this.loadResource()];
+                    case 1:
+                        _a.sent();
+                        return [4 /*yield*/, this.addEvents()];
+                    case 2:
+                        _a.sent();
+                        return [4 /*yield*/, this.loadMenu()];
+                    case 3:
+                        _a.sent();
+                        return [2 /*return*/];
+                }
+            });
+        });
+    };
+    Scene.prototype.mount = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, this.showMenu()];
+                    case 1:
+                        _a.sent();
+                        return [4 /*yield*/, this.selectMenu()];
+                    case 2:
+                        _a.sent();
+                        return [2 /*return*/];
+                }
+            });
+        });
+    };
+    Scene.prototype.play = function () {
+        return __awaiter(this, void 0, void 0, function () { return __generator(this, function (_a) {
+            return [2 /*return*/];
+        }); });
+    };
+    Scene.prototype.loadResource = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                return [2 /*return*/, this.task.init()];
+            });
+        });
+    };
+    Scene.prototype.addEvents = function () {
+        return __awaiter(this, void 0, void 0, function () { return __generator(this, function (_a) {
+            return [2 /*return*/];
+        }); });
+    };
+    Scene.prototype.loadMenu = function () {
+        return __awaiter(this, void 0, void 0, function () { return __generator(this, function (_a) {
+            return [2 /*return*/];
+        }); });
+    };
+    Scene.prototype.showMenu = function () {
+        return __awaiter(this, void 0, void 0, function () { return __generator(this, function (_a) {
+            return [2 /*return*/];
+        }); });
+    };
+    Scene.prototype.selectMenu = function () {
+        return __awaiter(this, void 0, void 0, function () { return __generator(this, function (_a) {
+            return [2 /*return*/];
+        }); });
+    };
+    return Scene;
+}());
+
+var config = {};
+
+var Core = function (container) { return __awaiter(void 0, void 0, void 0, function () {
+    var scene;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0:
+                scene = new Scene(container, config);
+                // @ts-ignore
+                window.scene = scene;
+                return [4 /*yield*/, execHook(scene, 'init')];
+            case 1:
+                _a.sent();
+                return [4 /*yield*/, execHook(scene, 'mount')];
+            case 2:
+                _a.sent();
+                return [4 /*yield*/, execHook(scene, 'play')];
+            case 3:
+                _a.sent();
+                return [2 /*return*/, scene];
+        }
+    });
+}); };
+
 /// <reference path="../types/main.d.ts" />
 var container = document.getElementById('app');
+Core(container);
 
 },{}]},{},[1]);
