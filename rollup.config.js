4effa0aaae45b0fe1c5eba07c77acd3f1750bbef
kscript <1258@live.cn>
2020-05-20 20:54:27 +0800

    添加代码压缩

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,8 @@
-import typescript from "rollup-plugin-typescript";
-import replace from 'rollup-plugin-replace';
-import babel from "rollup-plugin-babel";
+import typescript from "rollup-plugin-typescript"
+import replace from 'rollup-plugin-replace'
+import babel from "rollup-plugin-babel"
+
+// import { uglify } from 'rollup-plugin-uglify'
 
 const env = process.env.NODE_ENV;
 const config = {
@@ -12,14 +14,17 @@ const config = {
     plugins: [
         typescript({
             "target": "es5",
-            "sourceMap": false
+            "sourceMap": true
         }),
         babel(),
         replace({
             ENV: JSON.stringify(process.env.NODE_ENV || 'production'),
         }),
     ].concat(
-        // process.env.NODE_ENV !== 'production' ? uglify({}, uglifyES) : []
+        // process.env.NODE_ENV !== 'production' 
+        // ? [] 
+        // : 
+        uglify()
     )
 };
 
